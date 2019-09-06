082859c58d849df42b7d5dfdbcd785c9ad492066
youknowcast <ykc@daycrift.net>
2019-09-07 00:01:47 +0900

    till 13.14

--- programming_elixir/part13/issues/mix.exs
+++ programming_elixir/part13/issues/mix.exs
@@ -6,6 +6,8 @@ defmodule Issues.MixProject do
       app: :issues,
       escript: escript_config,
       version: "0.1.0",
+      name: "Issues",
+      source_url: "https://github.com/youknowcast/training/programming_elixir/part13/issues",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -26,7 +28,9 @@ defmodule Issues.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       { :httpoison, "~> 1.5.1" },
-      { :poison, "~> 4.0.1" }
+      { :poison, "~> 4.0.1" },
+      { :ex_doc, "~> 0.21.1" },
+      { :earmark, "~> 1.4.0" }
     ]
   end
 
