bb78ce385aff62185f5da2043fe91eda1d0d7c38
youknowcast <ykc@daycrift.net>
2019-09-07 00:44:33 +0900

    start 14

---
+++ programming_elixir/part14/spawn2.ex
@@ -0,0 +1,25 @@
+defmodule Spawn2 do
+  def greet do
+    receive do
+      {sender, msg} ->
+        send sender, { :ok, "Hello, #{msg}" }
+    end
+  end
+end
+
+pid = spawn(Spawn2, :greet, [])
+send pid, { self, "World" }
+
+receive do
+  {:ok, message} ->
+    IO.puts message
+end
+
+# 以下は実行されない
+send pid, { self, "Kermit" }
+receive do
+  {:ok, message} ->
+    IO.puts message
+end
+
+
