9214b704a9dd940b460dac22076e8d040ef5cc90
youknowcast <ykc@daycrift.net>
2019-09-06 22:20:08 +0900

    till 13.11 but `mix test` fails.. iam going to fix it.

--- programming_elixir/part13/issues/lib/issues/cli.ex
+++ programming_elixir/part13/issues/lib/issues/cli.ex
@@ -1,5 +1,6 @@
 defmodule Issues.CLI do
   @default_count 4
+  import Issues.TableFormatter, only: [ print_table_for_columns: 2 ]
 
   @moduledoc """
   Handle the command line parsing and the dispatch to
@@ -20,11 +21,13 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
-  def process({user, project, _count}) do
+  def process({user, project, count}) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response
     |> convert_to_list_of_maps
     |> sort_into_ascending_order
+    |> Enum.take(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
   end
 
   def convert_to_list_of_maps(list) do
