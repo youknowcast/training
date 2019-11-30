37c98c474e9ebccd8dbb12dcc66d067c90bdfa66
youknowcast <ykc@daycrift.net>
2019-11-30 17:48:37 +0900

    tdd 21 end

--- tdd/chapter2/xunit.py
+++ tdd/chapter2/xunit.py
@@ -6,14 +6,26 @@ class TestCase:
     def tearDown(self):
         pass
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return result
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+    def summary(self):
+        return "%d run, 0 failed" % self.runCount
 
 class WasRun(TestCase):
     def testMethod(self):
         self.log = self.log + "testMethod "
+    def testBrokenMethod(self):
+        raise Exception
     def setUp(self):
         self.log = "setUp "
     def tearDown(self):
@@ -24,6 +36,16 @@ class TestCaseTest(TestCase):
         test = WasRun("testMethod")
         test.run()
         assert("setUp testMethod tearDown " == test.log)
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary())
 
 
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
+#TestCaseTest("testFailedResult").run()
