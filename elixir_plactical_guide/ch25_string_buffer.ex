f1107feb30387de36731d85cfff8557066c7a17d
Ambe Hideki <ykc@daycrift.net>
2023-01-07 10:47:35 +0900

    feat: Add tests for ch25_string_buffer

--- elixir_plactical_guide/ch25_string_buffer.ex
+++ elixir_plactical_guide/ch25_string_buffer.ex
@@ -14,10 +14,43 @@ defmodule StringBuffer do
   end
 end
 
-StringBuffer.start_link("")
-StringBuffer.append("A")
-StringBuffer.append("B")
-StringBuffer.append("C")
-StringBuffer.append("D")
+#StringBuffer.start_link("")
+#StringBuffer.append("A")
+#StringBuffer.append("B")
+#StringBuffer.append("C")
+#StringBuffer.append("D")
+#
+#IO.inspect(StringBuffer.get_value())
 
-IO.inspect(StringBuffer.get_value())
+ExUnit.start(exclude: :not_implemented)
+
+defmodule TestStringBuffer do
+  use ExUnit.Case
+  import StringBuffer
+
+  defp initial_state(), do: ""
+
+  setup do
+    start_link(initial_state())
+
+    %{}
+  end
+
+  test "returns state" do
+    assert(get_value() == "")
+  end
+
+  test "returns updated state" do
+    append("hoge")
+    assert(get_value() == "hoge")
+
+    append("huga")
+    assert(get_value() == "hogehuga")
+  end
+
+  test "multi run start_link" do
+    {:error, {:already_started, pid}} = start_link("aa")
+    assert(is_pid(pid))
+  end
+end
+
