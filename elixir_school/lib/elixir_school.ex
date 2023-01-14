020eaf91686499a48e0798e04a98e0b2d95acf28
Ambe Hideki <ykc@daycrift.net>
2023-01-14 19:01:16 +0900

    feat: add project

---
+++ elixir_school/lib/elixir_school.ex
@@ -0,0 +1,18 @@
+defmodule ElixirSchool do
+  @moduledoc """
+  Documentation for `ElixirSchool`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirSchool.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ elixir_school/test/ch01_basics_test.exs
@@ -0,0 +1,32 @@
+defmodule Ch01BasicsTest do
+  use ExUnit.Case
+
+  test "2+3 =5" do
+    assert(2 + 3 == 5)
+  end
+
+  test "bit, oct, hex" do
+    assert(0b0110 == 6)
+    assert(0o644 == 420)
+    assert(0x1F == 31)
+  end
+
+  test "float" do
+    assert(3.14 == 3.14)
+    assert(Float.parse(".14") == :error)
+  end
+
+  test "atom, bool" do
+    assert(true == true)
+    assert(false == false)
+    assert(is_atom(true) == true && is_atom(false) == true)
+    assert(is_boolean(true) == true && is_boolean(false) == true)
+
+    defmodule Foo do
+    end
+
+    assert(is_atom(Foo) == true)
+    # not declared module also be atom
+    assert(is_atom(Bar) == true)
+  end
+end
